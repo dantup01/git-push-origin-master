49717e482d8f6e4fef2d6837f6c5fd63ab29cd39
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-01-23 16:02:02 +0000

    updated app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,25 @@ class ArticlesController < ApplicationController
   def edit; end
 
   def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
