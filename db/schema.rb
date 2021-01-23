2d1f3173ce3fcb9722ca731abbc642c8bf36ad90
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-01-23 16:42:13 +0000

    added image column to migration@

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_141404) do
+ActiveRecord::Schema.define(version: 2021_01_23_162857) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "articles" because of following StandardError
+#   Unknown type 'image' for column 'image'
 
 end
